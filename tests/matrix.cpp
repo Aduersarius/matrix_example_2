e88ffb9b4be37d64d18c58adcbe9666b0c99f4ec
Aduersarius <31540232+Aduersarius@users.noreply.github.com>
2018-06-11 19:35:48 +0300

    Update matrix.cpp

--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -5,7 +5,7 @@
 
 TEST_CASE("creating matrix")
 {
-    matrix_t matrix;
+    matrix_t<int> matrix;
     REQUIRE( matrix.rows() == 0 );
     REQUIRE( matrix.collumns() == 0 );
 }
@@ -17,7 +17,7 @@ TEST_CASE("reading matrix")
         "1 1 1\n"
         "2 2 2\n"
         "3 3 3" };
-    matrix_t matrix;
+    matrix_t<int> matrix;
     std::istringstream istream{ input };
     
     REQUIRE( matrix.read( istream ) );
@@ -49,7 +49,7 @@ TEST_CASE("add matrix")
         "3 3 3\n"
         "3 3 3\n"
         "3 3 3" };
-      matrix_t matrix_1, matrix_2;
+      matrix_t<int> matrix_1, matrix_2;
     std::istringstream istream_1{ input_1 };
     std::istringstream istream_2{ input_2 };
     REQUIRE(matrix_1.read(istream_1));
@@ -92,7 +92,7 @@ TEST_CASE("operator -")
         "1 1 1" };
 
     
-    matrix_t matrix_1,matrix_2;
+    matrix_t<int> matrix_1,matrix_2;
     std::istringstream istream1{ input_1 };
     std::istringstream istream2{ input_2 };
     REQUIRE( matrix_1.read( istream1 ) );
@@ -130,7 +130,7 @@ TEST_CASE("operator *")
         "6 6 6\n"
         "6 6 6" };
 
-    matrix_t matrix_1,matrix_2;
+    matrix_t<int> matrix_1,matrix_2;
     std::istringstream istream1{ input_1 };
     std::istringstream istream2{ input_2 };
     REQUIRE( matrix_1.read( istream1 ) );
@@ -170,7 +170,7 @@ TEST_CASE("operator +=")
         "3 3 3\n"
         "3 3 3" };
 
-    matrix_t matrix_1,matrix_2;
+    matrix_t<int> matrix_1,matrix_2;
     std::istringstream istream1{ input_1 };
     std::istringstream istream2{ input_2 };
     REQUIRE( matrix_1.read( istream1 ) );
@@ -210,7 +210,7 @@ TEST_CASE("operator -=")
         "1 1 1\n"
         "1 1 1" };
 
-    matrix_t matrix_1,matrix_2;
+    matrix_t<int> matrix_1,matrix_2;
     std::istringstream istream1{ input_1 };
     std::istringstream istream2{ input_2 };
     REQUIRE( matrix_1.read( istream1 ) );
@@ -250,7 +250,7 @@ TEST_CASE("operator *=")
         "6 6 6\n"
         "6 6 6" };
 
-    matrix_t matrix_1,matrix_2;
+    matrix_t<int> matrix_1,matrix_2;
     std::istringstream istream1{ input_1 };
     std::istringstream istream2{ input_2 };
     REQUIRE( matrix_1.read( istream1 ) );
@@ -261,4 +261,38 @@ TEST_CASE("operator *=")
     matrix_1.write( ostream );
     REQUIRE( input == ostream.str() );
 
+}
+TEST_CASE("typetests"){
+
+     std::string input{
+
+        "3, 3\n"
+        "1 1 1\n"
+        "2 2 2\n"
+        "3 3 3" };
+
+    matrix_t<int> matrix;
+    matrix_t<float> matrix1;
+    matrix_t<double> matrix2;
+
+    std::istringstream istream{ input };
+    std::istringstream istream1{ input };
+    std::istringstream istream2{ input };
+
+    REQUIRE( matrix.read( istream ) );
+    REQUIRE( matrix.rows() == 3 );
+    REQUIRE( matrix.collumns() == 3 );
+    REQUIRE( matrix1.read( istream1 ) );
+    REQUIRE( matrix1.rows() == 3 );
+    REQUIRE( matrix1.collumns() == 3 );
+    REQUIRE( matrix2.read( istream2 ) );
+    REQUIRE( matrix2.rows() == 3 );
+    REQUIRE( matrix2.collumns() == 3 );
+
+    std::ostringstream ostream;
+    matrix.write( ostream );
+    matrix1.write(ostream);
+    matrix2.write(ostream);
+
+    REQUIRE( input+input+input == ostream.str() );
 }
